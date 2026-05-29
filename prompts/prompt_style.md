6c1f336a9e48bb1b77c54172bd77e1b8aace75e8
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2026-05-29 19:54:52 +0530

    Learned About different prompting styles

---
+++ prompts/prompt_style.md
@@ -0,0 +1,41 @@
+# PROMPT STYLES
+
+## Alpaca Prompt
+### Instruction: <SYSTEM_PROMPT>\n
+### Input: <USER_QUERY>
+### Response: \n
+
+<!-- EXAMPLE
+
+Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{instruction}
+
+### Input:
+{input}
+
+### Response:
+
+ -->
+
+
+# ChatML
+## Used by ChatGPT , gemini etc
+### Most Widly Used!!!
+ <!--   
+        {
+            "role" : "system" | "user" | "assistant"
+            "content" : "string"
+        } 
+-->
+
+
+# INST Prompting
+## Used by LLama 2 etc
+
+<!-- 
+
+    [INST] What is the time now? [/INST]
+
+ -->
--- prompts/zero.py
+++ prompts/zero.py
@@ -6,14 +6,14 @@ import os
 load_dotenv()
 
 client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY")
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    api_key=os.getenv("GEMINI_API_KEY")
 )
 
 SYSTEM_PROMPT = "You are expert in Coding and only and only answer in coding related question. Do not ans anything else. If users ask something else, just say 'I am sorry, I can only answer coding related questions.' If user asks something else just say 'Sorry'."
 
 response = client.chat.completions.create(
-    model="gemma-4-31b-it-20260402:free",
+    model="gemini-3.5-flash",
     messages=[
         {
             "role" : "system",
